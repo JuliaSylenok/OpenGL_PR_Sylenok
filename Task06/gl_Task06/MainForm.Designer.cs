b1e8b7b6924610daebf138fb66edc12530af0b45
Julia Sylenok <silenokjulia1@gmail.com>
2024-12-25 15:16:02 +0200

    Add_Task06

---
+++ Task06/gl_Task06/MainForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace gl_Task06
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            renderControl1 = new RenderControl();
+            SuspendLayout();
+            // 
+            // renderControl1
+            // 
+            renderControl1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            renderControl1.BackColor = System.Drawing.Color.SlateGray;
+            renderControl1.dist = 0.2D;
+            renderControl1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 204);
+            renderControl1.ForeColor = System.Drawing.Color.White;
+            renderControl1.gridX = false;
+            renderControl1.gridY = false;
+            renderControl1.gridZ = false;
+            renderControl1.Location = new System.Drawing.Point(2, 3);
+            renderControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            renderControl1.Name = "renderControl1";
+            renderControl1.S = 0.2D;
+            renderControl1.Sb = 0.2D;
+            renderControl1.Size = new System.Drawing.Size(710, 428);
+            renderControl1.TabIndex = 0;
+            renderControl1.TextCodePage = 1251;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(713, 432);
+            Controls.Add(renderControl1);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            Name = "MainForm";
+            Text = "Main Form";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private RenderControl renderControl1;
+    }
+}
+
---
+++ Task06/gl_Task06/MainForm.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+using static gl_Task06.OpenGL;
+
+
+namespace gl_Task06
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
